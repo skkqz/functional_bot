8cd987bd65081a93cf1918959d355341ac565196
Andrew <skkqw@yandex.ru>
2023-04-18 18:55:34 +0300

    Добавление api конвертации валюты

--- loader.py
+++ loader.py
@@ -1,12 +1,9 @@
 import os
 from dotenv import load_dotenv
-import requests
-import json
 
 load_dotenv()
 
 BOT_API = os.getenv('BOT_API')
 OW_API = os.getenv('OW_API')
-
-
-test_api = f'http://api.openweathermap.org/geo/1.0/direct?q=Тверь&limit=5&appid={OW_API}'
+ERD_API = os.getenv('ERD_API')
+CURATE_API = os.getenv('CURATE_API')
--- xapi.py
+++ xapi.py
@@ -1,21 +1,41 @@
 import json
 import requests
 from typing import Dict
-from loader import OW_API
+from loader import OW_API, ERD_API, CURATE_API
 
 
-def get_weather_data(name_city: str, lan: str = 'ru', units: str = 'metric'):
+def get_weather_data(city: str, lan: str = 'ru', units: str = 'metric') -> Dict:
     """
     Получение данных о погоде в указанном городе.
-    :param name_city: Название города.
+    :param city: Название города.
     :param lan: Выбор языка для вывода данных.
     :param units: Параметр для меры измерения.
     :return: Словарь с данными о погоде.
     """
 
-    url_api = f'https://api.openweathermap.org/data/2.5/weather?q={name_city}&lang={lan}&appid={OW_API}&units={units}'
-    response = requests.get(url_api)
-    print(response.text)
+    url_api = f'https://api.openweathermap.org/data/2.5/weather?q={city}&lang={lan}&appid={OW_API}&units={units}'
+    try:
+        response = requests.get(url_api)
+        response_json = json.loads(response.text)
+        return response_json
+
+    except (Exception, TimeoutError) as _ex:
+        print(f'Произошла ошибка {_ex}.')
 
 
-get_weather_data('Тверь')
+def get_conversion_data(currency_from: str = 'USD', currency_to: str = 'RUB', amount: float = 1):
+
+    url_api = f"https://api.apilayer.com/exchangerates_data/convert?to=" \
+              f"{currency_to}&from={currency_from}&amount={amount}"
+    headers = {
+        'apikey': ERD_API
+    }
+
+    try:
+        response = requests.get(url=url_api, headers=headers, data={})
+        print(response.text)
+    except (Exception, TimeoutError) as _ex:
+        print(f'Произошла ошибка {_ex}')
+
+
+get_conversion_data(amount=5)
